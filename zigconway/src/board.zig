64058f434271ffe9acd4ca59569624a8fd8c2ff9
Tim Whelan <tim.whelan@ravelin.com>
2018-10-22 21:48:10 +0100

    Working on the board

--- zigconway/src/board.zig
+++ zigconway/src/board.zig
@@ -1,2 +1,18 @@
-#
-    
+/// A struct for storing a conway board
+
+pub const Board = struct. {
+    width: u32,
+    height: u32,
+
+    cells: []u8,
+
+    pub fn init(w: u32, h: u32) Board {
+        var b = Board.{
+            .width = w,
+            .height = h, 
+            .cells = undefined
+        };
+
+        return b;
+    }
+};
--- zigconway/src/main.zig
+++ zigconway/src/main.zig
@@ -1,7 +1,9 @@
 const warn = @import("std").debug.warn;
-const Board = @import("./board.zig");
+const Board = @import("./board.zig").Board;
 
 pub fn main() void {
     warn("conway\n");
+
+    var b = Board.init(20, 20);
 }
 
